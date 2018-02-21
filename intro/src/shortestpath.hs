99af62b01fb30e6a4b8ed1717b598798c7168a27
Hackle Wayne <Hackle.Wayne@TradeMe.local>
2018-02-21 18:06:12 +1300

    for remote

--- intro/src/shortestpath.hs
+++ intro/src/shortestpath.hs
@@ -7,15 +7,15 @@ import Control.Applicative
 -- shortest = head . (sortOn sum) . paths
 
 -- paths :: Matrix Int -> Maybe [[Int]]
-paths = downs 1 1
 
-downs :: Int -> Int -> Matrix Int -> Maybe [[Int]]
-downs x y mx = nexts <|> Just [[]] where
+paths :: Int -> Int -> Matrix Int -> Maybe [[Int]]
+paths x y mx = nexts <|> Just [[]] where
     nexts = do
         v <- safeGet x y mx
-        ds <- downs x (y+1) mx
-        return ((v :) <$> ds)
+        ys <- paths x (y+1) mx
+        xs <- paths (x+1) y mx
+        return (((v :) <$> ys) ++ ((v :) <$> xs))
 
 -- test
 sample = fromLists [[ 5..7], [1, 3, 3], [5, 0, 2]]
-result = downs 1 1 sample
+result = paths 1 1 sample
