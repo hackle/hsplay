bac4aed83c56040cc18152a0c8976a08886bbda3
Hackle & Hincal <devtesthackleandhincal@trademe.co.nz>
2019-04-13 08:27:10 +1200

    fold for segs

--- intro/src/lens.hs
+++ intro/src/lens.hs
@@ -16,6 +16,9 @@ data Segment = Segment
     } deriving (Show)
 makeLenses ''Segment
 
+extremityCoordinates :: Traversal Segment Segment Point Point
+extremityCoordinates a2fb s = Segment <$> a2fb (_segmentStart s) <*> a2fb (_segmentEnd s)
+
 sequ :: [ Maybe a ] -> Maybe [a]
 sequ [] = Just []
 sequ (x:xs) = (:) <$> x <*> (sequ xs)
@@ -31,3 +34,14 @@ mySet l a s = myOver l (const a) s
 
 myOver :: ((a -> Identity b) -> s -> Identity t) -> (a -> b) -> s -> t
 myOver l f s = runIdentity $ l (Identity . f) s
+
+data Fct a = F1 | F2 a
+
+instance Functor Fct where
+    fmap f F1 = F1
+    fmap f (F2 a) = F2 (f a)
+
+-- contra
+fco :: Applicative f => (a -> f b) -> a -> f b
+fco = ($)
+
